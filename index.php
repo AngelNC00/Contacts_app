9c851c224614cea0d2077ccef6f5b4fd4776675a
AngelNC <navarretec.angel@gmail.com>
2023-12-30 00:57:30 -0600

    Programa Finalizado Contacts_app

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require "partials/header.php" ?>
+
+<div class="welcome d-flex align-items-center justify-content-center">
+  <div class="text-center">
+  <br>
+    <h1>Almacene sus contactos ahora</h1>
+    <br>
+    <a class="btn btn-lg btn-dark" href="register.php">¡Agregar Ahora!</a>
+    <br><br>
+  </div>
+</div>
+
+<?php require "partials/footer.php" ?>
